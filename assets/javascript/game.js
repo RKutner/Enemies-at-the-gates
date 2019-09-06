145e28170f33782c512e62309633d13859eec096
Rob Kutner <Rob.Kutner@gmail.com>
2019-09-05 19:24:23 -0700

    got ninja working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,39 +2,41 @@ var wins = 0;
 var loss = 0;
 var e_str = 0;
 var p_str = 0;
-var ninja = 0;
-var paladin = 0;
-var summoner = 0;
-var scholar = 0;
+var nnj = 0;
+var pldn = 0;
+var smmnr = 0;
+var schlr = 0;
 
 
 
-$(document).ready(reset())
-
-function reset() {
-    ninja = (Math.floor(Math.random() * 15));
-    paladin = (Math.floor(Math.random() * 15));
-    summoner = (Math.floor(Math.random() * 15));
-    scholar = (Math.floor(Math.random() * 15));
-    e_str = ((Math.floor(Math.random() * 50)) + 50);
-    p_str = 0;
-}
-
-
-
-$(document).on('click', '.btn', function () {
-    console.log(e_str)
-    console.log(p_str)
-    var power = $(this).val();
-    console.log(power)
-    p_str += power
-    if (p_str === e_str) {
-        wins++
-        reset()
-    } else if (p_str > e_str) {
-        loss++
-        reset()
+$(document).ready(function () {
+    function reset() {
+        $('#ninja').val(Math.floor(Math.random() * 15))//.val is a function, and needs ()
+        $('#paladin').val((Math.floor(Math.random() * 15)));
+        $('#summoner').val((Math.floor(Math.random() * 15)));
+        $('#scholar').val(Math.floor(Math.random() * 15));
+        e_str = ((Math.floor(Math.random() * 50)) + 50);
+        p_str = 0;
     }
+
+    $(document).on('click', '.btn', function (event) {
+        console.log(e_str)
+        console.log(p_str)
+        var power = parseInt($(this).val());
+        console.log(power)
+        p_str += power
+        if (p_str === e_str) {
+            wins++
+            reset()
+        } else if (p_str > e_str) {
+            loss++
+            reset()
+        }
+    })
+
+    reset(); 
+
 })
+
 
 
